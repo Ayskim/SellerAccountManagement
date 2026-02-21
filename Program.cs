8efc110a7b8d48b4cb4022f4f237e7887ccf8bb7
Ayskim <homboykimberlyjaneparnaso@gmail.com>
2026-02-21 17:29:39 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SellerManagement 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SELLER ACCOUNT MANAGEMENT");
+
+            char choose;
+
+            Console.Write("1. Create new seller account\n2. View existing seller accounts\n3. Update a seller account\n4. Delete a seller account\n5. Exit\n");
+
+            choose = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            switch (choose)
+            {
+                case '1':
+                    Console.WriteLine("Add a account");
+                    break;
+
+                case '2':
+                    Console.WriteLine("Search account: ");
+                    break;
+
+                case '3':
+                    Console.WriteLine("Update Information: ");
+                    break;
+
+                case '4':
+                    Console.WriteLine("Delete account: ");
+                    break;
+
+                case '5':
+                    Console.WriteLine("Exit.");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input. Select an option.");
+                    break;
+            }
+        }
+    }
+}
