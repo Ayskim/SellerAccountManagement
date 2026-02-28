8a37be69d0c70691892f3fae330a95eb30074495
Ayskim <homboykimberlyjaneparnaso@gmail.com>
2026-02-28 16:19:02 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@ namespace SellerManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SELLER ACCOUNT MANAGEMENT");
+            Console.WriteLine("SHOPEE ACCOUNT MANAGEMENT");
+            Console.WriteLine("Please set up your account. Set now");
 
             char choose;
 
-            Console.Write("1. Create new seller account\n2. View existing seller accounts\n3. Update a seller account\n4. Delete a seller account\n5. Exit\n");
+            Console.WriteLine("\nACCOUNT & SECURITY");
+            Console.Write("1. Create account.\n2. Search name \n3. Update information.\n4. Delete your account\n5. Exit\n");
 
             choose = Console.ReadKey().KeyChar;
             Console.WriteLine();
@@ -18,19 +20,65 @@ namespace SellerManagement
             switch (choose)
             {
                 case '1':
-                    Console.WriteLine("Add a account");
+                    Console.WriteLine("ADD ACCOUNT");
+
+                    Console.WriteLine("\nEnter Seller Name: ");
+                    string name = Console.ReadLine();
+
+                    Console.WriteLine("Enter email address: ");
+                    string emailAdd = Console.ReadLine();
+
+                    Console.WriteLine("Enter phone number: ");
+                    string phoneNum = Console.ReadLine();
+
+                    Console.WriteLine("Enter present address: ");
+                    string preAdd = Console.ReadLine();
+
+                    Console.WriteLine("\nCreated Successfully.");
+
                     break;
 
                 case '2':
-                    Console.WriteLine("Search account: ");
+                    Console.WriteLine("\nSEARCH ACCOUNT");
+
+                    Console.WriteLine("Enter...");
+                    string searchName = Console.ReadLine();
+
+                    Console.WriteLine("Searching account...");
+
                     break;
 
                 case '3':
-                    Console.WriteLine("Update Information: ");
+                    Console.WriteLine("UPDATE INFORMATION");
+
+                    Console.WriteLine("\nMy Profile");
+                    Console.WriteLine("\nName: ");
+                    Console.WriteLine("Username: ");
+                    Console.WriteLine("Bio: ");
+                    Console.WriteLine("Gender");
+                    Console.WriteLine("Birthday: ");
+                    Console.WriteLine("Phone: ");
+                    Console.WriteLine("Email: ");
                     break;
 
                 case '4':
-                    Console.WriteLine("Delete account: ");
+                    Console.WriteLine("REQUEST ACCOUNT DELETION");
+
+                    Console.WriteLine("Do you want to delete your account (Yes/No): ");
+
+                    string ans = Console.ReadLine().ToLower();
+
+                    if(ans == "yes")
+                    {
+                        Console.WriteLine("Please Note. Account Deletion is Irreversable");
+                        Console.WriteLine("Proceed to Delete. Deleting...");
+                    }else if (ans == "no"){
+                        Console.WriteLine("Cancelled Account Deletion");
+                    }else
+                    {
+                        Console.WriteLine("Invalid Input");
+                    }
+
                     break;
 
                 case '5':
